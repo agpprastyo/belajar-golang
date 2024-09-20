27262c7789a0f5118c3fe29d068b68fbac05ee27
agpprastyo <prasetyo.agpr@gmail.com>
2024-09-21 05:52:33 +0700

    add snippetBox

--- freecodecamp/foreverstore/main.go
+++ freecodecamp/foreverstore/main.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
 func main() {
 	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
 	// to see how GoLand suggests fixing it.
@@ -20,6 +17,3 @@ func main() {
 		fmt.Println("i =", 100/i)
 	}
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
---
+++ freecodecamp/foreverstore/p2p/transport.go
@@ -0,0 +1,10 @@
+package p2p
+
+// Peer is an interface that represents the remote nodes
+type Peer interface {
+}
+
+// Transport is anything that handles the communication
+// between the nodes in the network.
+type Transport interface {
+}
